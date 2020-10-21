e5f2a71fe7a0e3aeb0840fb5ebfcda32536b56da
Eduardo Barbosa Oliveira da Silva <eduardobos@jmalucelliti.com.br>
2020-10-21 13:14:40 -0300

    Implementacoes do dia 2

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
 app.use(express.json());
 
 app.post("/teste", (request, response) => {
     console.log(request.body);
+    
 });
 
 app.listen(3333);
